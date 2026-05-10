b01e60b443129a7872505b31ee28a261ad4de8b3
Dendensig <sigmand12@gmail.com>
2026-05-09 23:54:06 -0400

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,1 +1,14 @@
 
+export default function Portfolio() {
+  return (
+    <section className="section dark">
+      <h2>Recent Work</h2>
+
+      <div className="grid">
+        <div className="mock-image">Project 1</div>
+        <div className="mock-image">Project 2</div>
+        <div className="mock-image">Project 3</div>
+      </div>
+    </section>
+  );
+}
