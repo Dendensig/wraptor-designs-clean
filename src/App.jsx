9428bb9b5b89e02418f815172d01c1b9654418a5
Dendensig <sigmand12@gmail.com>
2026-05-09 23:25:03 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="hero">
+      <h1>WRAPTOR DESIGNS</h1>
+      <p>Premium Vinyl Graphics & Vehicle Wraps</p>
+      <a href="tel:12692779151">Call Dennis: (269) 277-9151</a>
+    </div>
+  )
+}
